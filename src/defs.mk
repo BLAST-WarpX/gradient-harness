f0c861d740bd09edd8fd831eb193eff1a3708d17
markabate <mabate@utexas.edu>
2026-06-11 17:22:26 -0700

    Add square function

---
+++ src/defs.mk
@@ -0,0 +1,9 @@
+# Global Make variables
+
+# Change these if necessary 
+LLVM_DIR = /opt/homebrew/opt/llvm@16
+ENZYME_DIR = $(HOME)/src/Enzyme
+
+CC = $(LLVM_DIR)/bin/clang
+CXX = $(LLVM_DIR)/bin/clang++
+
---
+++ src/square/square.h
@@ -0,0 +1,8 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+double square(double x);
+double dsquare(double x);
+
+#endif
+
