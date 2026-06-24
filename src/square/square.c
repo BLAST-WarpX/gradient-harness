f1abf6dc631b5a37a767e4e91b78716dab634e21
markabate <mabate@utexas.edu>
2026-06-24 08:11:00 -0700

    Add python tests for square example

--- src/square/square.c
+++ src/square/square.c
@@ -1,6 +1,7 @@
-#include "square.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-#include "stdio.h"
+#include "square.h"
 
 double __enzyme_autodiff(void*, double);
 
@@ -12,15 +13,16 @@ double dsquare(double x) {
     return __enzyme_autodiff((void*) square, x);
 }
 
-int main() {
-    double test_vals[] = {1.0, 2.0, 4.5, 10};
-    int n = sizeof(test_vals) / sizeof(test_vals[0]);
-
-    for (int i = 0; i < n; i++) {
-        double x = test_vals[i];
-        printf("x = %8.2f; square(x) = %8.2f; dsquare(x) = %8.2f\n", x, square(x), dsquare(x));
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        return 0;
     }
 
+    double x = strtod(argv[1], NULL);
+    double dx = dsquare(x);
+
+    printf("%f\n", dx);
+    
     return 0;
 }
 
