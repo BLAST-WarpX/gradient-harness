a50880de945e03f0dbc15bb6e23c18873fd3ba68
markabate <mabate@utexas.edu>
2026-07-06 15:49:16 -0700

    Update Makefiles to run on SUSE Linux and macOS

--- src/defs.mk
+++ src/defs.mk
@@ -1,9 +1,29 @@
 # Global Make variables
-
-# Change these if necessary 
-LLVM_DIR = /opt/homebrew/opt/llvm@16
+UNAME = $(shell uname)
+ 
+LLVM_VERSION ?= 16
 ENZYME_DIR = $(HOME)/src/Enzyme
+ENZYME_PLUGIN_EXT = so
 
-CC = $(LLVM_DIR)/bin/clang
-CXX = $(LLVM_DIR)/bin/clang++
+# Use LLVM_DIR environment variable if set
+ifeq ($(UNAME), Linux)
+	LINUX_DIST = $(shell lsb_release -is)
 
+	ifeq ($(LINUX_DIST), SUSE)
+		# Perlmutter
+		LLVM_INSTALL_DIR ?= $(shell spack location -i llvm@$(LLVM_VERSION))
+	endif 
+else ifeq ($(UNAME), Darwin)
+	# macOS
+	LLVM_INSTALL_DIR ?= $(shell brew --prefix)/opt/llvm@$(LLVM_VERSION)
+	ENZYME_PLUGIN_EXT = dylib
+endif
+
+LLVM_INSTALL_DIR ?= /usr/lib/llvm-$(LLVM_VERSION)
+
+CC = $(LLVM_INSTALL_DIR)/bin/clang
+CXX = $(LLVM_INSTALL_DIR)/bin/clang++
+
+# Builtin commands
+print-%:
+	@echo '$*=$($*)'
