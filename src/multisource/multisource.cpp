57fcd5954f5fd59ff57cc5e6810076d619bc03fb
markabate <mabate@utexas.edu>
2026-07-17 11:52:05 -0700

    Update multisource example to use Enzyme C++ template syntax

--- src/multisource/multisource.cpp
+++ src/multisource/multisource.cpp
@@ -2,16 +2,19 @@
 
 #include "math_utils.h"
 
-int enzyme_const, enzyme_out;
-double __enzyme_autodiff(void*, ...);
+// Note: extern is required for enzyme_* variables to work in multisource project 
+extern int enzyme_const, enzyme_out, enzyme_dup;
+
+template <typename RT, typename... T>
+RT __enzyme_autodiff(void*, T...);
 
 int main() {
     double x = 2.0;
     double y = 3.0;
 
-    double dmult_dx = __enzyme_autodiff((void*) mult, x, y);
+    double dmult_dy = __enzyme_autodiff<double>((void*) mult, enzyme_const, x, enzyme_out, y);
 
-    std::cout << dmult_dx << "\n";
+    std::cout << dmult_dy << "\n";
 
     return 0;
 }
