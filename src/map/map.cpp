ae237b6a98ed1d5975af45c78db9df0348d84a79
markabate <mabate@utexas.edu>
2026-07-21 09:41:57 -0700

    Add std::map example

---
+++ src/map/map.cpp
@@ -0,0 +1,24 @@
+#include <map> 
+#include <iostream>
+
+using namespace std;
+
+extern int enzyme_const, enzyme_dup, enzyme_out;
+
+template <typename RT, typename... T>
+RT __enzyme_autodiff(void*, T...);
+
+double test_func(double x) {
+    map<int, int> m{{1, 10}};
+
+    return x;
+}
+
+int main() {
+    double x = 3.0;
+    double dx = __enzyme_autodiff<double>((void*)test_func, enzyme_out, x);
+
+    cout << dx << "\n";
+    
+    return 0;
+}
